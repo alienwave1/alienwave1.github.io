60c4e4bd92977effd71a8a710352abbec1cc5d2c
Alien Wave <istominmartin@gmail.com>
2019-07-06 21:12:18 +0300

    changes

---
+++ photostock/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+
+app.use(express.static(__dirname + "/")); // css file
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'images/');
+    },
+
+    filename: function (req, file, cb) {
+        cb(null, file.originalname); // file.jpg
+    }
+});
+
+app.use(multer({ storage: storage }).single("filedata"));
+
+app.post("/upload", function (req, res, next) {
+    let filedata = req.file;
+
+    if (!filedata) res.send("Ошибка при загрузке файла!");
+
+    else res.send("Файл загружен");
+});
+
+app.set("view engine", "ejs");
+
+app.use("/", function (req, res) {
+    res.render("index", {
+        title: 'Hey'
+    });
+});
+
+app.listen(5500, function () {
+    console.log('Server started on port: 5500');
+});
