e0361400df7068e9709b73d993b13c7137dae167
Alien Wave <istominmartin@gmail.com>
2019-07-07 18:43:08 +0300

    changes

--- photostock/index.js
+++ photostock/index.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const app = express();
 const multer = require('multer');
+const fs = require('fs');
 const port = 5550;
+const testFolder = './images/';
 
 app.use(express.static(__dirname + "/")); // css file
-
-app.set("view engine", "ejs");
-
-app.use("/", function (req, res) {
-    res.render("index", {
-        title: "",
-    });
-});
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -25,12 +19,28 @@ var storage = multer.diskStorage({
 
 app.use(multer({ storage: storage }).single("filedata"));
 
-app.post("/upload", function (req, res, next) {
+app.post("/", function (req, res, next) {
     let filedata = req.file;
+    console.log(filedata);
 
     if (!filedata) res.send("Ошибка при загрузке файла!");
 
     else res.send("Файл загружен");
+});
+
+var imgs = fs.readdirSync(testFolder, (err, files) => {
+    files.forEach((file) => {
+        file = [file];
+        return file;
+    });
+});
+
+app.set("view engine", "ejs");
+
+app.use("/", function (req, res) {
+    res.render("index", {
+        title: imgs,
+    });
 });
 
 app.listen(port, function () {
