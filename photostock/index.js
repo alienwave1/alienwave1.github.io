e57aa46e72b0d1c07b782f57b780db468b45e6b9
Alien Wave <istominmartin@gmail.com>
2019-07-09 22:21:35 +0300

    changes

--- photostock/index.js
+++ photostock/index.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 const fs = require('fs');
+const crypto = require('crypto');
+const sqlite3 = require('sqlite3').verbose();
 const port = process.env.PORT || 5550;
+const imgsFolder = './images/';
 
 app.use(express.static(__dirname + "/")); // css file
 app.use(express.static(__dirname + "/images"));
+
+let db = new sqlite3.Database('./db/db_main.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log('Connected to the main SQlite database.');
+});
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -35,23 +46,22 @@ app.use(multer({ storage: storage, fileFilter: fileFilter }).single("filedata"))
 
 app.post("/", function (req, res, next) {
     let filedata = req.file;
-    //let maxSize = 8388608; // 8388608 = 1 mb in bites
+    let maxSize = 8388608; // 8388608 = 1 mb in bites
 
     console.log(filedata);
     res.redirect("/");
 
-    /*
-    if (!filedata) res.send("Ошибка при загрузке файла!");
+    // Сделать нормальное отображение ошибок!!
+    if (!filedata) console.log("Ошибка при загрузке файла!");
 
-    else if (filedata.size > maxSize) res.send("Недопустимый размер файла! Максимум 1 МБ");
+    else if (filedata.size > maxSize) console.log("Недопустимый размер файла! Максимум 1 МБ");
 
-    else res.send("Файл загружен");
-    */
+    else console.log("Файл загружен.");
+    
 });
 
 function checkFile() {
     try {
-        let imgsFolder = './images/';
         let imgs = fs.readdirSync(imgsFolder);
 
         fs.statSync(imgsFolder);
@@ -74,7 +84,23 @@ function checkFile() {
     }
 }
 
+function hashFiles() {
+    var check = fs.readdirSync(imgsFolder);
+    var hashesArr = [];
+
+   /* fs.createReadStream(imgsFolder + check.toString())
+        .pipe(crypto.createHash('md5').setEncoding('hex'))
+        .on('finish', function () {
+            let hashes = this.read();
+            hashesArr.push(hashes);
+            console.log(hashesArr);
+            console.log(hashes); //the hash
+        });
+*/
+}
+
 checkFile();
+hashFiles();
 
 app.set("view engine", "ejs");
 
