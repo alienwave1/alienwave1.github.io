26ead2ad8013ec1382b8dc356644298cfe800826
Alien Wave <istominmartin@gmail.com>
2019-07-12 19:01:31 +0300

    changes

--- photostock/index.js
+++ photostock/index.js
@@ -8,9 +8,10 @@ const port = process.env.PORT || 5550;
 const imgsFolder = './images/';
 
 app.use(express.static(__dirname + "/")); // css file
-app.use(express.static(__dirname + "/images"));
 
-let db = new sqlite3.Database('./db/db_main.db', sqlite3.OPEN_READWRITE, (err) => {
+/* -------- DATABASE -------- */
+
+var db = new sqlite3.Database('./db/db_main.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         return console.error(err.message);
     }
@@ -18,63 +19,56 @@ let db = new sqlite3.Database('./db/db_main.db', sqlite3.OPEN_READWRITE, (err) =
     console.log('Connected to the main SQlite database.');
 });
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "images/");
-    },
+/* -------- FUNCTIONS -------- */
 
-    filename: function (req, file, cb) {
-        cb(null, file.originalname); // file.jpg
-    }
-});
+function hashFiles(cb) { // Хеширование файлов
+    let imgsFiles = fs.readdirSync(imgsFolder);
 
-var fileFilter = (req, file, cb) => {
-    if (file.mimetype === "image/png" ||
-        file.mimetype === "image/jpg" ||
-        file.mimetype === "image/jpeg") {
-        cb(null, true);
+    fs.statSync(imgsFolder);
 
-    }
-
-    else {
-        cb(null, false);
-        console.log(`Недопустимое расширение файла: ${file.mimetype}`);
-    }
+    imgsFiles.forEach(arr => {
+        fs.createReadStream(imgsFolder + arr)
+            .pipe(crypto.createHash('md5').setEncoding('hex'))
+            .on('finish', function () {
+                let hashes = this.read();
+                cb(hashes); // Отправка коллбека во внешний мир
+            });
+    })
 }
 
-app.use(multer({ storage: storage, fileFilter: fileFilter }).single("filedata"));
+function checkFile(cb) {
+    let sql = `SELECT hash FROM hashes`;
 
-app.post("/", function (req, res, next) {
-    let filedata = req.file;
-    let maxSize = 8388608; // 8388608 = 1 mb in bites
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
 
-    console.log(filedata);
-    res.redirect("/");
+        rows.forEach((row) => {
+            console.log(row.hash);
+            cb(row.hash);
+        });
+    });
+}
 
-    // Сделать нормальное отображение ошибок!!
-    if (!filedata) console.log("Ошибка при загрузке файла!");
-
-    else if (filedata.size > maxSize) console.log("Недопустимый размер файла! Максимум 1 МБ");
-
-    else console.log("Файл загружен.");
-    
-});
-
-function checkFile() {
+function checkFolder() { // Проверка на существующую папку
     try {
         let imgs = fs.readdirSync(imgsFolder);
 
         fs.statSync(imgsFolder);
+        return imgs; // Отправляет на клиента
+
+        /*
+        // Бессмысленный вариант
+        var imgs = fs.readdirSync(imgsFolder, (err, files) => {
+            files.forEach((file) => {
+                file = [file];
+                return file;
+            });
+        });
+
         return imgs;
-
-        // var imgs = fs.readdirSync(imgsFolder, (err, files) => {
-        //     files.forEach((file) => {
-        //         file = [file];
-        //         return file;
-        //     });
-        // });
-
-        // return imgs;
+        */
     }
 
     catch (e) {
@@ -84,32 +78,90 @@ function checkFile() {
     }
 }
 
-function hashFiles() {
-    var check = fs.readdirSync(imgsFolder);
-    var hashesArr = [];
+checkFolder();
 
-   /* fs.createReadStream(imgsFolder + check.toString())
-        .pipe(crypto.createHash('md5').setEncoding('hex'))
-        .on('finish', function () {
-            let hashes = this.read();
-            hashesArr.push(hashes);
-            console.log(hashesArr);
-            console.log(hashes); //the hash
-        });
-*/
+/* -------- UPLOAD -------- */
+
+var storage = multer.diskStorage({ // Download to folder /images
+    destination: function (req, file, cb) {
+        cb(null, "images/");
+    },
+
+    filename: function (req, file, cb) {
+        cb(null, file.originalname); // Format: file.jpg
+    }
+});
+
+var fileFilter = (req, file, cb) => { // Также сделать проверку, основанную на хэше.
+    if (file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/jpeg") {
+        cb(null, true);
+    }
+
+    else {
+        cb(null, false);
+        console.log(`Недопустимое расширение файла: ${file.mimetype}`);
+    }
 }
 
-checkFile();
-hashFiles();
+var upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: { fileSize: 300000 }
+}).single("filedata");
+
+app.post("/", upload, function (req, res, next) { // Обработка запроса
+    let filedata = req.file;
+    //let maxSize = 8388608; // 8388608 = 1 mb in bites
+
+    console.log(filedata);
+    res.redirect("/");
+
+    /*
+    hashFiles(cb => { // Если файл успешно загружен - хэш идёт в базу данных
+        let hashesArr = [];
+
+        hashesArr.push(cb);
+        console.log(hashesArr);
+
+        let placeholders = hashesArr.map((hashesAr) => '(?)').join(',');
+        let sql = "INSERT INTO hashes(hash) VALUES " + placeholders;
+        console.log(sql);
+
+        db.run(sql, hashesArr, function (err) { // Добавление хеша в БД
+            if (err) {
+                return console.error(err.message);
+            }
+
+            console.log(`Rows inserted ${this.changes}`);
+        });
+    });
+    console.log("Файл загружен.");
+    */
+});
+
+app.use(function (err, req, res, next) { // Обработчик ошибки
+    if (err.code === 'LIMIT_FILE_SIZE') {
+        console.log("File is too big");
+        res.redirect("/");
+    }
+
+    // Handle any other errors
+})
+
+/* -------- VIEW -------- */
 
 app.set("view engine", "ejs");
 
 app.use("/", function (req, res) {
     res.render("index", {
-        title: checkFile(),
+        title: checkFolder(),
     });
 });
 
 app.listen(port, function () {
     console.log(`Server started on port: ${port}`);
+
+    checkFile();
 });
