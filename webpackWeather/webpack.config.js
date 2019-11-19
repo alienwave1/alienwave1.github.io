fc58350efc66107d2116ea6c93196b0f906d2a17
WebDev <webdev@iMac.local>
2019-11-19 17:30:51 +0300

    changes

---
+++ webpackWeather/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
+    },
+    mode: 'development',
+    entry: './src/client.js',
+    output: {
+        filename: 'client.js',
+        path: path.resolve(__dirname, 'dist'),
+    }
+};
